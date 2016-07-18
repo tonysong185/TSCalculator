5ca13e9cc3c2194b40a9cfe2c78c68a890024a75
TonySongWD <tonysong072@gmail.com>
2016-07-18 09:31:57 +0800

    Add enum Operation to brain.

--- TSCalculator/TSCalculator/TSCalculatorBrain.swift
+++ TSCalculator/TSCalculator/TSCalculatorBrain.swift
@@ -8,18 +8,42 @@
 
 import Foundation
 
+func multiply(op1: Double, op2: Double) -> Double {
+    return op1 * op2
+}
+
 class TSCalculatorBrain {
+    
     private var accumulator = 0.0
     
     func setOperand(operand: Double) {
         accumulator = operand
     }
     
+    var operations: Dictionary<String, Operation> = [
+        "π" : .Constant(M_PI), // M_PI,
+        "e" : .Constant(M_E), // M_E,
+        "√" : .UnaryOperation(sqrt), // sqrt
+        "cos" : .UnaryOperation(cos), // cos
+        "×" : .BinaryOperation(multiply),
+        "=" : .Equals
+    ]
+    
+    enum Operation {
+        case Constant(Double)
+        case UnaryOperation((Double) -> Double)
+        case BinaryOperation((Double, Double) -> Double)
+        case Equals
+    }
+    
     func performOperation(symbol: String) {
-        switch symbol {
-        case "π": accumulator = M_PI
-        case "√": accumulator = sqrt(accumulator)
-        default: break
+        if let operation = operations[symbol] {
+            switch operation {
+            case .Constant(let value): accumulator = value // the value just a parm name, you can name any
+            case .UnaryOperation(let function): accumulator = function(accumulator) // equal up
+            case .BinaryOperation : break
+            case .Equals : break
+            }
         }
     }
     
